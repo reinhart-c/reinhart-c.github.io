627fd872f30cf99c217d38fa65850accf605dd00
ReinhartC <reinhart.chr@gmail.com>
2024-05-17 20:25:28 +0700

    add project page and bug fixes

--- js/project.js
+++ js/project.js
@@ -1,1 +1,26 @@
-gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
+const el = document.querySelector('#project');
+const fx = new TextScramble(el);
+fx.setText("PROJECT");
+
+gsap.registerPlugin(ScrollTrigger);
+let tween;
+let carrousel = document.querySelector(".carrousel");
+let panels = gsap.utils.toArray(".projectItem");
+let scrollFunc = ScrollTrigger.getScrollFunc(window);
+tween = gsap.to(panels, {
+	xPercent: -100 * ( panels.length - 1 ),
+	ease: "none",
+	scrollTrigger: {
+		trigger: ".carrousel",
+		pin: true,
+		start: "top top",
+		scrub: 1,
+        anticipatePin: 1,
+		snap: {
+			snapTo: 1 / (panels.length - 1),
+			inertia: false,
+			duration: {min: 0.1, max: 0.1}
+		},
+		end: () =>  "+=" + (carrousel.offsetWidth - innerWidth)
+	}
+});
